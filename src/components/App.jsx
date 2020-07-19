321bae3604eec6c5282f28c553880d4daaeecc53
Mukul <mukul.ajay.1499@gmail.com>
2020-07-19 18:17:10 +0530

    v2 inital commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,15 +3,17 @@ import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note"
 import notes from "../notes"
-
+import CreateArea from "./CreateArea"
 function App(){
-    return  <div >
-                <Header/>
-                    <div className="pin-board">
-                        {notes.map(note => (<Note key={note.key} title={note.title} content={note.content}/>))}
-                    </div>
-                <Footer/>
-            </div>;
+    return  (
+    <div >
+        <Header/>
+            <CreateArea />
+                <div className="pin-board">
+                    {notes.map(note => (<Note key={note.key} title={note.title} content={note.content}/>))}
+                </div>
+        <Footer/>
+    </div>);
 }
 
 export default App;
